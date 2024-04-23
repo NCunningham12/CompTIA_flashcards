48d0f8d8536c8c5de5e88b27e3f986f9c3e5cb8f
Nathan Cunningham <counterweight22@gmail.com>
2024-04-22 23:48:16 -0700

    Initial commit. Game description written in comments. No code yet

---
+++ flashcards.py
@@ -0,0 +1,6 @@
+# The goal here is to build a flashcards generator in Python to study for CompTIA Core 1
+# The game will prompt a question
+# The user will push enter to be shown the answer
+# The user will be asked if they got it correct or not
+# If correct, the question will not be asked again
+# If incorrect, the question will be asked again until correct
